08d458b57d6ff128a3e10c1da03b4a7fa98ca5f7
Anusha <93347485+Anushameena-S@users.noreply.github.com>
2024-03-16 09:05:14 +0530

    Add files via upload

---
+++ practice_sql_aggregate_functions.sql
@@ -0,0 +1,81 @@
+USE hr_emp;
+
+
+-- -------------------------------------
+-- Aggregate functions
+-- -------------------------------------
+
+-- Min
+-- MAx
+-- Sum
+-- Avg
+-- Count
+
+Select min(salary) from employees;
+Select max(salary) from employees;
+Select sum(salary) from employees;
+Select avg(salary) from employees;
+Select count(salary) from employees;
+Select count(Distinct salary) from employees;
+select count(*) from employees;
+select count(commission_pct) from employees;
+
+select * from employees;
+
+Select avg(salary) from employees
+where department_id = 70;
+
+Select min(salary) from employees
+where department_id = 70;
+
+-- Aggregate with group by  clause
+select avg(salary) from employees;
+ 
+select * from employees;
+
+select department_id, avg(salary) 
+from employees
+group by department_id;
+
+-- Multiple Grouping Columns
+select department_id, hire_date, sum(salary), avg(salary)
+from employees group by department_id, hire_date;
+
+
+-- Aggregate with where and group by clause 
+select department_id, avg(salary) 
+from employees
+where department_id is not null
+group by department_id;
+
+select * from employees;
+
+select department_id, job_id, avg(salary) 
+from employees
+where department_id is not null
+group by department_id, job_id;
+
+select department_id, min(salary) 
+from employees
+group by department_id;
+
+-- Aggregate with having clause 
+select department_id, min(salary) 
+from employees
+group by department_id
+having min(salary)>=7000;
+
+
+select department_id, avg(salary) 
+from employees
+where department_id is not null
+group by department_id
+having avg(salary)>10000;
+
+select department_id, sum(salary) 
+from employees
+where department_id is not null
+group by department_id
+having sum(salary) is not null;
+
+-- order - select from, where, group by, having, order by, limit
